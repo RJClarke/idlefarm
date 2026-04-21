b6a6e7b970b42d5d56e5bc3be9d8a271f6c52f3f
RJ Clarke <RJClarke4@gmail.com>
2026-04-20 22:20:14 -0500

    feat: add SettingsManager with ShowFloatingNumbers PlayerPrefs setting

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public static class SettingsManager
+{
+    private const string KEY_FLOATING_NUMBERS = "setting_floating_numbers";
+
+    private static int? _showFloatingNumbers;
+
+    public static bool ShowFloatingNumbers
+    {
+        get
+        {
+            if (_showFloatingNumbers == null)
+                _showFloatingNumbers = PlayerPrefs.GetInt(KEY_FLOATING_NUMBERS, 1);
+            return _showFloatingNumbers.Value == 1;
+        }
+        set
+        {
+            _showFloatingNumbers = value ? 1 : 0;
+            PlayerPrefs.SetInt(KEY_FLOATING_NUMBERS, _showFloatingNumbers.Value);
+        }
+    }
+}
